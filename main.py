faf86e3bdeba420c895719add3a70c2faed5444a
Jack Sullivan <jhaldisul@gmail.com>
2021-06-22 13:03:47 -0400

    sdfsdfs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-
+from concurrent.futures import ProcessPoolExecutor
 from eeg import EEGParser
 from vr import VRParser
 
@@ -10,29 +10,34 @@ vr_host = "localhost"
 vr_port = 8080
 
 
-#eeg = EEGParser(eeg_host, eeg_port)
+eeg = EEGParser(eeg_host, eeg_port)
 
-#future_eeg_data = eeg.get_data()
+future_eeg_data = eeg.get_data()
 
-#df = future_eeg_data.result()
+df = future_eeg_data.result()
 
-#df.to_csv("eeg_data")
+df.to_csv("eeg_data.csv")
 
-async def run_eeg():
+
+
+'''
+def run_eeg():
     eeg = EEGParser(eeg_host, eeg_port)
     eeg.parse_data()
     return eeg.df
     #print("EEG is totally connected and running")
     #eturn "super real eeg data"
 
+def run_vr():
 
 vr = VRParser(vr_host, vr_port)
 server = vr.run_server()
 
 loop = asyncio.get_event_loop()
-tasks = run_eeg(), server
+tasks = run_eeg()
 
-eeg_data, vr_data = loop.run_until_complete(asyncio.gather(*tasks))
+eeg_data = loop.run_until_complete(asyncio.gather(*tasks))
 loop.run_forever()
 
-print(f"VR data: {vr_data}")
+eeg_data.to_csv("eeg_data.csv")
+'''
